d0446e220d695bc1d2b9e83faa9f2dc4aca413b8
843207296@qq.com <chenhouzhen@yy.com>
2020-11-02 00:34:05 +0800

    配置

--- blog-admin-api/src/main/java/me/chenhz/api/BlogAdminApiApplication.java
+++ blog-admin-api/src/main/java/me/chenhz/api/BlogAdminApiApplication.java
@@ -1,7 +1,9 @@
 package me.chenhz.api;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 //开启事务
 @EnableTransactionManagement
