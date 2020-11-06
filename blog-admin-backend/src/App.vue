550a8ee0e9be1b0efe8ac9af9ca718300a5389ee
843207296@qq.com <chenhouzhen@yy.com>
2020-11-06 23:09:52 +0800

    前端

--- blog-admin-backend/src/App.vue
+++ blog-admin-backend/src/App.vue
@@ -1,33 +1,11 @@
 <template>
   <div id="app">
-    <router-view/>
-    <!-- <footers></footers> -->
+    <router-view />
   </div>
 </template>
 
 <script>
-import Footers from '@/components/Footers'
-
 export default {
-  name: 'App',
-  components:{
-    Footers
-  }
+  name: 'App'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-* {
-    padding: 0;
-    margin: 0;
-}
-</style>
